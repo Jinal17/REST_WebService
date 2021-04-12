638a54955d18ea9bca7f3a07205d99e1974b8480
Jinal <kunteshbharucha@kunteshs-mbp.myfiosgateway.com>
2021-04-12 00:07:30 -0400

    CORS fix

--- src/main/java/com/swe645/controller/StudentController.java
+++ src/main/java/com/swe645/controller/StudentController.java
@@ -19,6 +19,7 @@ public class StudentController {
         return service.saveStudent(student);
     }
 
+    @CrossOrigin(origins="http://localhost:53812")
     @PostMapping("/addStudents")
     public List<Student> addStudents(@RequestBody List<Student> students) {
         return service.saveStudents(students);
@@ -30,21 +31,25 @@ public class StudentController {
         return service.getStudents();
     }
 
+    @CrossOrigin(origins="http://localhost:53812")
     @GetMapping("/studentById/{id}")
     public Student findStudentById(@PathVariable int id) {
         return service.getStudentById(id);
     }
 
+    @CrossOrigin(origins="http://localhost:53812")
     @GetMapping("/student/{name}")
     public Student findStudentByName(@PathVariable String name) {
         return service.getStudentByName(name);
     }
 
+    @CrossOrigin(origins="http://localhost:53812")
     @PutMapping("/update")
     public Student updateStudent(@RequestBody Student student) {
         return service.updateStudent(student);
     }
 
+    @CrossOrigin(origins="http://localhost:53812")
     @DeleteMapping("/delete/{id}")
     public String deleteStudent(@PathVariable int id) {
         return service.deleteStudent(id);
